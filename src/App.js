66182088372a70bbcef2eaf536071d24da68b4a0
HagerMagdy2 <hager9671@gmail.com>
2025-01-06 22:56:40 +0200

    list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 import MyfirstComponent from './MyfirstComponent';
 
 function App() {
+  const tasks=[
+    "task1",
+    "task2",
+    "task3",
+    "task4",
+    "task5"
+  ]
+  const myTasksList = tasks.map(task =>{
+    return <li key={task}>{task}</li>
+  })
+  const showCategories = true;
   const Today = new Date().toLocaleDateString();
   return (
     <div className="App">
     <MyfirstComponent name={Today}/>
+    <div>
+      <ul>
+        {myTasksList}
+      </ul>
     </div>
+    </div>
+  
   );
 }
 
--- src/MyfirstComponent.js
+++ src/MyfirstComponent.js
@@ -1,9 +1,8 @@
 import "./MyfirstComponentStyle.css"
-export default function MyfirstComponent({name}) {
-  
+export default function MyfirstComponent(name) {
   return (
     <div>
-      <h1 className="active" style={{backgroundColor:"red"}}>Hello {name}!</h1>
+      <h1 className="active" style={{backgroundColor:"red"}}>Hello {name.name}!</h1>
       <button onClick={sayHello}>Say Hello</button>
     </div>
   );
