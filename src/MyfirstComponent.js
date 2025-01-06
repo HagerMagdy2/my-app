6da37ba78506d821f671fec74a546cfb09d180eb
HagerMagdy2 <hager9671@gmail.com>
2025-01-06 19:56:18 +0200

    styles and classes

--- src/MyfirstComponent.js
+++ src/MyfirstComponent.js
@@ -1,3 +1,12 @@
+import "./MyfirstComponentStyle.css"
 export default function MyfirstComponent() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1 className="active" style={{backgroundColor:"red"}}>Hello World!</h1>
+      <button onClick={sayHello}>Say Hello</button>
+    </div>
+  );
+}
+function sayHello(){
+  alert('Hello from MyFirstComponent!');
 }
