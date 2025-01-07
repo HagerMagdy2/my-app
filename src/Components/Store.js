cecde47ba614f6d50bb3004772804da94d5b146e
HagerMagdy2 <hager9671@gmail.com>
2025-01-07 17:09:40 +0200

    useRef

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { useReducer } from "react";
+import { useReducer,useRef } from "react";
 
 function Store() {
   let initialState = 0;
-
+  const username=useRef();
+  const password=useRef();
   const [count, dispatch] = useReducer(reduce, initialState);
   function reduce(state, action) {
     if (action === "INCREMENT") {
@@ -14,12 +15,25 @@ function Store() {
       return state;
     }
   }
+function  handel(e){
+  e.preventDefault();
+
+ 
+console.log(username.current.value);
+console.log(password.current.value);
+  }
 
   return (
     <div>
       the count is {count}{" "}
       <button onClick={() => dispatch("INCREMENT")}>increase</button>
       <button onClick={() => dispatch("DECREMENT")}>decrese</button>
+      <form onSubmit={handel}>
+      <input type="text" placeholder="name" ref={username} />
+      <input type="text" placeholder="password" ref={password} />
+      <button type="submit">Submit</button>
+      </form>
+    
     </div>
   );
 }
